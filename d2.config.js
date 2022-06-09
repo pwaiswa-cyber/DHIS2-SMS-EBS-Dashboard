899ad64d849bff35541d8a430cda366934959be7
pwaiswa-cyber <70855678+pwaiswa-cyber@users.noreply.github.com>
2022-06-09 23:30:08 +0300

    Add files via upload

---
+++ d2.config.js
@@ -0,0 +1,13 @@
+const config = {
+    type: 'app',
+    name: 'EBS',
+    title: 'EBS',
+    description: 'Manage all received surveillance signals',
+    coreApp: false,
+
+    entryPoints: {
+        app: './src/App',
+    },
+}
+
+module.exports = config
